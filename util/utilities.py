04a986ade670e336fd5e5a7b7b453ea19065dcfb
YashM8 <yashmali0326@gmail.com>
2024-03-10 14:47:25 -0700

    added a util

--- util/utilities.py
+++ util/utilities.py
@@ -9,5 +9,17 @@ def indent_json(file_path, indent=4):
         json.dump(data, f, indent=indent)
 
 
+def print_keys(json_file_path, artifact):
+    # Load the JSON file
+    with open(json_file_path, 'r') as file:
+        data = json.load(file)
+
+    # Get the keys under "observations" -> "default_1"
+    artifact = data.get("observations", {}).get(artifact, {}).keys()
+
+    # Print the keys
+    for key in artifact:
+        print(key)
 
 
+print_keys("../measured_data/test1.json", "default_1")
